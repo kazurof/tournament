db09f765271bf561d48658d1df6e3183dea893e7
kazuro.fukuhara <kazuro.fukuhara@gmail.com>
2016-02-17 00:13:10 +0900

    add type parameter

--- src/main/java/tournament/AnalyseMain.java
+++ src/main/java/tournament/AnalyseMain.java
@@ -7,12 +7,15 @@ import tournament.logic.ResultType;
 public class AnalyseMain {
   public static void main(String[] args) {
     int numOfGame = 3;
-    if (args.length != 0) {
+    if (1 <= args.length ) {
       numOfGame = Integer.parseInt(args[0]);
     }
-
+    ResultType type = ResultType.RAW_VALUE;
+    if (2 <= args.length ) {
+      type = ResultType.valueOfShortName(args[1]);
+    }
 
     int[][] result = Calc.analyseFromTournamentDataFile(numOfGame);
-    Calc.generateResultToFile(result, ResultType.FRACTION_IN_FACTORED);
+    Calc.generateResultToFile(result, type);
   }
 }
