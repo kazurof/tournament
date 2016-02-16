56e92e697f001ce497af91663b9a7a34d57e0700
kazuro.fukuhara <kazuro.fukuhara@gmail.com>
2016-02-17 00:10:03 +0900

    add valueOfShortName() method.

--- src/main/java/tournament/logic/ResultType.java
+++ src/main/java/tournament/logic/ResultType.java
@@ -1,5 +1,19 @@
 package tournament.logic;
 
 public enum ResultType {
-  RAW_VALUE, FRACTION, FRACTION_IN_FACTORED
+  RAW_VALUE, FRACTION, FRACTION_IN_FACTORED;
+
+  public static ResultType valueOfShortName(String name) {
+
+    if ("raw".equals(name)) {
+      return RAW_VALUE;
+    }
+    if ("frac".equals(name)) {
+      return FRACTION;
+    }
+    if ("factor".equals(name)) {
+      return FRACTION_IN_FACTORED;
+    }
+    return RAW_VALUE;
+  }
 }
